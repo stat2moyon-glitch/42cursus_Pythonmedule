4bfa3d3512a8fb8f91746140e0d1c62cfc10f369
stat2moyon-glitch <2t.moyo.n@gmail.com>
2026-09-21 19:36:29 +0900

    Implement get_player_pos function for coordinates
    Add function to get player coordinates from input.
    Change indent from 2 to 4 spaces

--- module03/ex02/ft_coordinate_system.py
+++ module03/ex02/ft_coordinate_system.py
@@ -1,1 +1,32 @@
 
+def get_player_pos() -> tuple[float, float, float]:
+  while True:
+    user_input = input("Enter new coordinates as floats in format 'x,y,z': ")
+    parts = user_input.split(",")
+    count = len(parts)
+    if count != 3: 
+      print("Invalid syntax")
+      continue
+      
+    numbers :list[float] = []
+    
+      try:
+        for temp in parts:
+        number = float(temp)
+        numbers.append(number)
+      except ‎ValueError as error:
+        print(f"Error on parameter {temp}: {error}")
+        continue
+      break
+    coordinates = tuple(numbers)
+    return coordinates
+        
+        
+  
+          
+        
+        
+      
+    break
+  print(parts)
+  
