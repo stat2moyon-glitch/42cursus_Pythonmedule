0e2b410b313b91db45f69371ea083f4300adccb1
stat2moyon-glitch <2t.moyo.n@gmail.com>
2026-09-22 17:32:48 +0900

    Refactor coordinate input and add distance calculation
    Refactor get_player_pos function for better readability and structure. Add calculate_distance function to compute distance between two 3D points.

--- module03/ex02/ft_coordinate_system.py
+++ module03/ex02/ft_coordinate_system.py
@@ -1,32 +1,55 @@
+math.sqrt()
 
 def get_player_pos() -> tuple[float, float, float]:
-  while True:
-    user_input = input("Enter new coordinates as floats in format 'x,y,z': ")
-    parts = user_input.split(",")
-    count = len(parts)
-    if count != 3: 
-      print("Invalid syntax")
-      continue
-      
-    numbers :list[float] = []
-    
-      try:
-        for temp in parts:
-        number = float(temp)
-        numbers.append(number)
-      except ‎ValueError as error:
-        print(f"Error on parameter {temp}: {error}")
-        continue
-      break
+    while True:
+        user_input = input("Enter new coordinates as floats in format 'x,y,z': ")
+        parts = user_input.split(",")
+        count = len(parts)
+        if count != 3: 
+            print("Invalid syntax")
+            continue
+            
+        numbers :list[float] = []
+        
+        try:
+            for temp in parts:
+                number = float(temp)
+                numbers.append(number)
+        except ‎ValueError as error:
+            print(f"Error on parameter {temp}: {error}")
+            continue
+    break
     coordinates = tuple(numbers)
     return coordinates
-        
-        
-  
-          
-        
-        
-      
-    break
-  print(parts)
-  
+
+def calculate_distance(
+    pos1: tuple[float, float, float],
+    pos2: tuple[float, float, float],
+) -> float:
+    x1, y1, z1 = pos1
+    x2, y2, z2 = pos2
+
+    distance = math.sqrt(
+        (x2 - x1)**2
+        + (y2 - y1)**2
+        + (z2 - z1)**2
+    )
+
+def main() -> None:
+    print("=== Game Coordinate System ===")
+    pos1 = get_player_pos()
+    print(f"Got a first set of tuple: {pos1}")
+    x, y, z = pos1
+    print(f"It includes: X={x}, Y={y}, Z={z}")
+    center = (0.0, 0.0, 0.0)    
+    distance = calculate_distance(pos1, center)
+    print(f"Distance to center: {distance:.4f}")
+
+    print("Get a second set of coordinates")
+    pos2 = get_player_pos()
+    distance = calculate_distance(pos1, pos2)
+    print(
+        f"Distance between the 2 sets of coordinates: "
+        f"{distance:.4f}"
+    )
+    
